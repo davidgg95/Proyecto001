ea2f567371fb0aa8773e859219f08fa5990b1a8c
Your Name <you@example.com>
2020-10-01 12:40:10 +0200

    quinto commit

---
+++ doc/ejercicio001.ts
@@ -0,0 +1,7 @@
+let nombre:string = "David González";
+let edad:number   = 25;
+const PERSONAJE: { nombre:string, edad:number } = {
+    nombre: nombre,
+    edad: edad
+}
+console.log(PERSONAJE);
---
+++ doc/ejercicio002.ts
@@ -0,0 +1,3 @@
+let resultadoDoble = (a:number , b:number) => (a + b )* 2;
+
+console.log(resultadoDoble(2,2));
